658e5e5c2d6d4a38418e2d014a8c059ad668a5e3
gpetrini <gpetrinidasilveira@gmail.com>
2018-12-24 10:16:05 -0200

    overwritten

--- setup.py
+++ setup.py
@@ -8,23 +8,23 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name='pysolve',
-    version='0.1.7',
+    name='pysolve3',
+    version='0.1.1.2',
     packages=find_packages(),
-    install_requires=['sympy', 'numpy'],
+    install_requires=['sympy==1.1', 'numpy'],
     license='MIT',
-    author='Kenn Takara',
-    author_email='kenn.takara@outlook.com',
+    author='Gabriel Petrini da Silveira',
+    author_email='gpetrinidasilveira@gmail.com',
     classifiers={
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering :: Mathematics',
     },
-    url='https://github.com/kennt/pylinsolve',
+    url='https://github.com/gpetrini/pysolve3',
     long_description=read('README.rst'),
     description='A simple interface for solving systems of linear equations',
 )
